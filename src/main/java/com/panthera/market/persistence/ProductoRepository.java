37bbb3330df89d7236dce0de3243ca283bc97709
Daniel Leonardo Romero Albarracin <daniel.romero@DESKTOP-2TO21MO.pragma.com.co>
2020-10-05 11:02:55 -0500

    add query methods

--- src/main/java/com/panthera/market/persistence/ProductoRepository.java
+++ src/main/java/com/panthera/market/persistence/ProductoRepository.java
@@ -4,6 +4,7 @@ import com.panthera.market.persistence.crud.ProductoCrudRepository;
 import com.panthera.market.persistence.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
 
@@ -11,5 +12,13 @@ public class ProductoRepository {
 
     public List<Producto> getAll() {
         return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategria(int idCategoria) {
+        return productoCrudRepository.findByIdCategoriaByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad) {
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
     }
 }
--- src/main/java/com/panthera/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/panthera/market/persistence/crud/ProductoCrudRepository.java
@@ -1,7 +1,23 @@
 package com.panthera.market.persistence.crud;
 
 import com.panthera.market.persistence.entity.Producto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+
+    // With SQL Query
+    @Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
+    List<Producto> getByCategoria(int idCategoria);
+
+    // With Query Method
+    List<Producto> findByIdCategoria(int idCategoria);
+
+    List<Producto> findByIdCategoriaByNombreAsc(int idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
+
 }
