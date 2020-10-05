cb422a9d98a70c8000c4a71aced31c7c03fe305e
leolplex <leolplex@yahoo.com>
2020-10-05 14:43:39 -0500

    Fix unit testing and build to CircleCI

--- src/test/java/com/panthera/market/PantheraMarketApplicationTests.java
+++ src/test/java/com/panthera/market/PantheraMarketApplicationTests.java
@@ -1,13 +1,14 @@
 package com.panthera.market;
 
+import com.panthera.market.web.controller.PantheraMarketApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {PantheraMarketApplication.class})
 class PantheraMarketApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
