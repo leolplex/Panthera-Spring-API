224015b69d9d57ee2b836df390a0b2690a6056cc
leolplex <leolplex@yahoo.com>
2020-10-05 15:12:54 -0500

    Fix unit testing and build to CircleCI 2

--- src/test/java/com/panthera/market/PantheraMarketApplicationTests.java
+++ src/test/java/com/panthera/market/PantheraMarketApplicationTests.java
@@ -2,9 +2,12 @@ package com.panthera.market;
 
 import com.panthera.market.web.controller.PantheraMarketApplication;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = {PantheraMarketApplication.class})
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 class PantheraMarketApplicationTests {
 
     @Test
