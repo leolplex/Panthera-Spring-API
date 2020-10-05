675db52c47c23d1ab54edbc69dc16ce965a38b4d
Daniel Leonardo Romero Albarracin <daniel.romero@DESKTOP-2TO21MO.pragma.com.co>
2020-10-05 10:16:36 -0500

    map database relationship between class

--- src/main/java/com/panthera/market/persistence/entity/Categoria.java
+++ src/main/java/com/panthera/market/persistence/entity/Categoria.java
@@ -1,6 +1,7 @@
 package com.panthera.market.persistence.entity;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "categorias")
@@ -10,10 +11,11 @@ public class Categoria {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_categoria")
     private  Integer idCategoria;
-
     private  String descripcion;
-
     private  Boolean estado;
+
+    @OneToMany(mappedBy = "categoria")
+    private List<Producto> productos;
 
     public Integer getIdCategoria() {
         return idCategoria;
--- src/main/java/com/panthera/market/persistence/entity/ComprasProducto.java
+++ src/main/java/com/panthera/market/persistence/entity/ComprasProducto.java
@@ -1,19 +1,24 @@
 package com.panthera.market.persistence.entity;
 
-import javax.persistence.Column;
-import javax.persistence.EmbeddedId;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "compras_productos")
 public class ComprasProducto {
 
     @EmbeddedId
-    private  ComprasProductoPK id;
-    private  Integer cantidad;
+    private ComprasProductoPK id;
+    private Integer cantidad;
     private Double total;
     private Boolean estado;
+
+    @ManyToOne
+    @JoinColumn(name = "id_compra", insertable = false, updatable = false)
+    private Compra compra;
+
+    @ManyToOne
+    @JoinColumn(name = "id_producto", insertable = false, updatable = false)
+    private Producto producto;
 
     public ComprasProductoPK getId() {
         return id;
