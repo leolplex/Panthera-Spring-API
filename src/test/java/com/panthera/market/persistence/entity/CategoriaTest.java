503ee2d5203a706d71ebff551b7e2576ceae07c6
leolplex <leolplex@yahoo.com>
2020-10-06 13:50:23 -0500

    Add unit test in Categoria, Cliente

--- src/test/java/com/panthera/market/persistence/entity/CategoriaTest.java
+++ src/test/java/com/panthera/market/persistence/entity/CategoriaTest.java
@@ -1,4 +1,80 @@
 package com.panthera.market.persistence.entity;
 
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CategoriaTest {
+
+    @Test
+    public void InstanceCategoriaDefaultValues() {
+        // Arrange & Act
+        Categoria tester = new Categoria();
+
+        // Assert
+        assertNull(tester.getIdCategoria(), "getIdCategoria must be null");
+        assertNull(tester.getDescripcion(), "getDescripcion must be null");
+        assertNull(tester.getEstado(), "getEstado must be null");
+        assertNull(tester.getProductos(), "getEstado must be null");
+    }
+
+    @Test
+    public void getAndSetIdCategoria() {
+        // Arrange
+        Categoria tester = new Categoria();
+        final int idCategoria = 500;
+
+        //Act
+        tester.setIdCategoria(idCategoria);
+        final int getIdCategoria = tester.getIdCategoria();
+
+        // Assert
+        assertEquals(getIdCategoria, idCategoria, "setIdCategoria must be 500");
+    }
+
+    @Test
+    public void getAndSetDescripcion() {
+        // Arrange
+        Categoria tester = new Categoria();
+        final String descripcion = "My Category";
+
+        //Act
+        tester.setDescripcion(descripcion);
+        final String getDescripcion = tester.getDescripcion();
+
+        // Assert
+        assertEquals(getDescripcion, descripcion, "setDescripcion must be My Category");
+    }
+
+    @Test
+    public void getAndSetEstado() {
+        // Arrange
+        Categoria tester = new Categoria();
+        final boolean estado = true;
+
+        //Act
+        tester.setEstado(estado);
+        final boolean getEstado = tester.getEstado();
+
+        // Assert
+        assertEquals(getEstado, estado, "setEstado must be true");
+    }
+
+    @Test
+    public void getAndSetProductos() {
+        // Arrange
+        Categoria tester = new Categoria();
+        final List<Producto> productos = new ArrayList<>();
+
+        //Act
+        tester.setProductos(productos);
+        final List<Producto> getProductos = tester.getProductos();
+
+        // Assert
+        assertEquals(getProductos, productos, "setProductos must be []");
+    }
 }
--- src/test/java/com/panthera/market/persistence/entity/ClienteTest.java
+++ src/test/java/com/panthera/market/persistence/entity/ClienteTest.java
@@ -1,4 +1,125 @@
 package com.panthera.market.persistence.entity;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 public class ClienteTest {
+
+    @Test
+    public void InstanceClienteDefaultValues() {
+        // Arrange & Act
+        Cliente tester = new Cliente();
+
+        // Assert
+        assertNull(tester.getId(), "getId must be null");
+        assertNull(tester.getNombre(), "getNombre must be null");
+        assertNull(tester.getApellidos(), "getApellidos must be null");
+        assertNull(tester.getCelular(),  "getCelular must be null");
+        assertNull(tester.getDireccion(), "getDireccion must be null");
+        assertNull(tester.getCorreoElectronico(), "getCorreoElectronico must be null");
+        assertNull(tester.getCompras(), "getCompras must be null");
+    }
+
+    @Test
+    public void getAndSetId() {
+        // Arrange
+        Cliente tester = new Cliente();
+        final int id = 100;
+
+        //Act
+        tester.setId(id);
+        final int getId = tester.getId();
+
+        // Assert
+        assertEquals(getId, id, "setId must be 100");
+    }
+
+    @Test
+    public void getAndSetNombre() {
+        // Arrange
+        Cliente tester = new Cliente();
+        final String nombre = "My name";
+
+        //Act
+        tester.setNombre(nombre);
+        final String getNombre = tester.getNombre();
+
+        // Assert
+        assertEquals(getNombre, nombre, "setNombre must be My name");
+    }
+
+    @Test
+    public void getAndSetApellidos() {
+        // Arrange
+        Cliente tester = new Cliente();
+        final String apellidos = "My lastname";
+
+        //Act
+        tester.setApellidos(apellidos);
+        final String getApellidos = tester.getApellidos();
+
+        // Assert
+        assertEquals(getApellidos, apellidos, "setApellidos must be My lastname");
+    }
+
+    @Test
+    public void getAndSetCelular() {
+        // Arrange
+        Cliente tester = new Cliente();
+        final Long celular = 1234_5678_9012_3456L;
+
+        //Act
+        tester.setCelular(celular);
+        final Long getCelular = tester.getCelular();
+
+        // Assert
+        assertEquals(getCelular, celular, "setApellidos must be 1234_5678_9012_3456L");
+    }
+
+    @Test
+    public void getAndSetDireccion() {
+        // Arrange
+        Cliente tester = new Cliente();
+        final String direccion = "My address";
+
+        //Act
+        tester.setDireccion(direccion);
+        final String getDireccion = tester.getDireccion();
+
+        // Assert
+        assertEquals(getDireccion, direccion, "setDireccion must be My address");
+    }
+
+    @Test
+    public void getAndSetCorreoElectronico() {
+        // Arrange
+        Cliente tester = new Cliente();
+        final String correoElectronico = "myemail@email.com";
+
+        //Act
+        tester.setCorreoElectronico(correoElectronico);
+        final String getCorreoElectronico = tester.getCorreoElectronico();
+
+        // Assert
+        assertEquals(getCorreoElectronico, correoElectronico, "setCorreoElectronico must be myemail@email.com");
+    }
+
+    @Test
+    public void getAndSetCompras() {
+        // Arrange
+        Cliente tester = new Cliente();
+        final List<Compra> compras = new ArrayList<>();
+
+        //Act
+        tester.setCompras(compras);
+        final List<Compra> getCompras = tester.getCompras();
+
+        // Assert
+        assertEquals(getCompras, compras, "setCompras must be []");
+    }
 }
