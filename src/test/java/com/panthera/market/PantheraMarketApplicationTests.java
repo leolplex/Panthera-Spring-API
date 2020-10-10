946ce986e82dccfc59fcf5fe96c37c77840ef962
Daniel <raiga1234@gmail.com>
2020-10-09 20:55:52 -0500

    Fix main unit test

--- src/test/java/com/panthera/market/PantheraMarketApplicationTests.java
+++ src/test/java/com/panthera/market/PantheraMarketApplicationTests.java
@@ -1,19 +1,23 @@
-package com.panthera.market.web.controller;
+package com.panthera.market;
 
 import com.panthera.market.PantheraMarketApplication;
+import com.panthera.market.domain.service.ProductService;
+import com.panthera.market.persistence.crud.ProductoCrudRepository;
+import com.panthera.market.persistence.mapper.ProductMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest(classes = {PantheraMarketApplication.class})
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+//@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 class PantheraMarketApplicationTests {
+
 
     @Test
     void contextLoads() {
-//        String[] args = {};
-//        PantheraMarketApplication.main(args);
     }
 
 }
