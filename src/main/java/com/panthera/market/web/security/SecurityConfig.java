607b828f5f75e86576af9ecde4c9d67984258d6c
Daniel <raiga1234@gmail.com>
2020-10-13 18:46:11 -0500

    Remove code smell

--- src/main/java/com/panthera/market/web/security/SecurityConfig.java
+++ src/main/java/com/panthera/market/web/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -13,6 +14,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(pantheraUserDetailService);
+        auth.userDetailsService(pantheraUserDetailService).passwordEncoder(new BCryptPasswordEncoder());
     }
 }
