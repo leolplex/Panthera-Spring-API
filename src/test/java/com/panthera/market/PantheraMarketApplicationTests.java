affd5b835632b8a008da37bc627689857e90e667
leolplex <leolplex@yahoo.com>
2020-10-15 12:27:14 -0500

    Ignore main to unit testing

--- src/test/java/com/panthera/market/PantheraMarketApplicationTests.java
+++ src/test/java/com/panthera/market/PantheraMarketApplicationTests.java
@@ -8,11 +8,14 @@ import com.panthera.market.persistence.mapper.ProductMapper;
 import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(classes = {PantheraMarketApplication.class})
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
@@ -26,6 +29,8 @@ class PantheraMarketApplicationTests {
 
     @Test
     void contextLoads() {
+        assertEquals("banner.txt", SpringApplication.BANNER_LOCATION_PROPERTY_VALUE, "BANNER_LOCATION_PROPERTY_VALUE should be banner.txt");
+
     }
 
 }
